63993edea527ece9f73a252a448c61637e738aae
sangho527 <sy980527@naver.com>
2022-11-15 02:02:39 +0900

    [ADD] Comment기능 추가 221115

--- src/main/java/com/mustache/springbootmustache1111/controller/ArticleController.java
+++ src/main/java/com/mustache/springbootmustache1111/controller/ArticleController.java
@@ -2,8 +2,12 @@ package com.mustache.springbootmustache1111.controller;
 
 
 import com.mustache.springbootmustache1111.domain.dto.ArticleDto;
+import com.mustache.springbootmustache1111.domain.dto.CommentDto;
 import com.mustache.springbootmustache1111.domain.entity.Article;
+import com.mustache.springbootmustache1111.domain.entity.Comment;
+import com.mustache.springbootmustache1111.domain.entity.Hospital;
 import com.mustache.springbootmustache1111.repository.ArticleRepository;
+import com.mustache.springbootmustache1111.repository.CommentRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -18,12 +22,14 @@ import java.util.Optional;
 @Controller
 @RequestMapping("/articles")
 @Slf4j
-public class ArticleController { // 여기는 mustache등을 맵핑해주는 파일
+public class ArticleController {
 
-    private final ArticleRepository articleRepository; // repository를 di
+    private final ArticleRepository articleRepository;
+    private final CommentRepository commentRepository;
 
-    public ArticleController(ArticleRepository articleRepository) {
-        this.articleRepository = articleRepository;
+    public ArticleController(ArticleRepository repository, CommentRepository commentRepository) {
+        this.articleRepository = repository;
+        this.commentRepository = commentRepository;
     }
 
 
@@ -41,14 +47,22 @@ public class ArticleController { // 여기는 mustache등을 맵핑해주는 파
     @GetMapping("/{id}")
     public String selectSingle(@PathVariable Long id, Model model) {
         Optional<Article> optArticle = articleRepository.findById(id);
-
-        if (!optArticle.isEmpty()) {
-            // Optional.get() ---> Article
+        if(!optArticle.isEmpty()){
             model.addAttribute("article", optArticle.get());
+
+            List<Comment> comments = commentRepository.findByArticleId(id);
+            model.addAttribute("comments", comments);
             return "articles/show";
-        } else {
+        } else{
             return "articles/error";
         }
+    }
+
+    @PostMapping("/post")
+    public String articles(ArticleDto articleDto) {
+        log.info(articleDto.toString());
+        Article savedArticle = articleRepository.save(articleDto.toEntity());
+        return String.format("redirect:/articles/%d", savedArticle.getId());
     }
 
     @GetMapping("/{id}/edit")
@@ -76,6 +90,22 @@ public class ArticleController { // 여기는 mustache등을 맵핑해주는 파
         articleRepository.deleteById(id);
         return "redirect:/articles";
     }
+
+    @PostMapping("/comment/post")
+    public String comment(CommentDto commentDto) {
+        Comment savedComment = commentRepository.save(commentDto.toEntity());
+        return String.format("redirect:/articles/%d", savedComment.getArticleId());
+    }
+
+//    @GetMapping("/{id}")
+//    public String show(@PathVariable Long id, Model model) {
+//        Optional<Hospital> hospital = hospitalRepository.findById(id);
+//        List<Comment> comments = commentRepository.findByArticleId(id);
+//        log.info("comment cnt:{}", comments.size());
+//        model.addAttribute("hospital", hospital.get());
+//        model.addAttribute("comments", comments);
+//        return "articles/show";
+//    }
 
     @PostMapping("") // articles로 호출을 하면 인서트하는 부분
     public String createArticle(ArticleDto articleDto) { //Dto란 데이터 전송을 하는것
--- src/main/java/com/mustache/springbootmustache1111/domain/entity/Article.java
+++ src/main/java/com/mustache/springbootmustache1111/domain/entity/Article.java
@@ -4,6 +4,8 @@ package com.mustache.springbootmustache1111.domain.entity;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 
 import javax.persistence.*;
 
@@ -12,6 +14,7 @@ import javax.persistence.*;
 @Table(name = "article3")
 @NoArgsConstructor
 @AllArgsConstructor
+@Setter
 public class Article {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) // ID 생성을 DB에 맡김
