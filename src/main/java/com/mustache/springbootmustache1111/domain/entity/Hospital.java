407d8c2b04ebf36b8a1adc24f0cba94e53123c68
sangho527 <sy980527@naver.com>
2022-11-14 14:08:24 +0900

    [UPDATE] Create hospital api 221114

---
+++ src/main/java/com/mustache/springbootmustache1111/domain/entity/Hospital.java
@@ -0,0 +1,31 @@
+package com.mustache.springbootmustache1111.domain.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="nation_wide_hospitals")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Hospital {
+    @Id
+    private Integer id;
+
+    @Column(name = "hospital_name")
+    private String hospitalName;
+
+    @Column(name="road_name_address")
+    private String roadNameAddress;
+
+    private Integer patientRoomCount;
+    private Integer totalNumberOfBeds;
+}
