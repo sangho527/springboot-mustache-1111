c0cad067aa61bef851d21d3be354c2a31e9dd149
sangho527 <sy980527@naver.com>
2022-11-14 14:11:48 +0900

    [ADD] HospitalController 221114

---
+++ src/main/java/com/mustache/springbootmustache1111/controller/HospitalController.java
@@ -0,0 +1,35 @@
+package com.mustache.springbootmustache1111.controller;
+
+import com.mustache.springbootmustache1111.domain.entity.Hospital;
+import com.mustache.springbootmustache1111.repository.HospitalRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+
+@Controller
+@RequestMapping("/hospitals")
+@Slf4j
+public class HospitalController {
+    private final HospitalRepository hospitalRepository;
+
+    public HospitalController(HospitalRepository hospitalRepository) {
+        this.hospitalRepository = hospitalRepository;
+    }
+
+
+    @GetMapping("")
+    public String hospitalList(Model model, Pageable pageable) {
+        Page<Hospital> hospitals = hospitalRepository.findAll(pageable);
+        log.info("size:{}", hospitals.getSize());
+        model.addAttribute("hospitals", hospitals);
+        model.addAttribute("previous", pageable.previousOrFirst().getPageNumber());
+        model.addAttribute("next", pageable.next().getPageNumber());
+        return "hospital/list";
+    }
+}
