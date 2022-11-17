dc96f5ceb42765f06172fa08e6d793088375cf41
sangho527 <sy980527@naver.com>
2022-11-17 11:16:41 +0900

    [UPDATE] Repository 수정 221117

--- src/main/java/com/mustache/springbootmustache1111/repository/HospitalRepository.java
+++ src/main/java/com/mustache/springbootmustache1111/repository/HospitalRepository.java
@@ -1,14 +1,21 @@
 package com.mustache.springbootmustache1111.repository;
+
 import com.mustache.springbootmustache1111.domain.entity.Hospital;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface HospitalRepository extends JpaRepository<Hospital, Integer> {
-    List<Hospital> findByBusinessTypeNameIn(List<String> businessTypes);
+    List<Hospital> findByBusinessTypeNameIn(List<String> businessType);
     List<Hospital> findByBusinessTypeNameInAndRoadNameAddressLike(List<String> businessType, String nameAddress);
+
     List<Hospital> findByRoadNameAddressContaining(String keyword); // 포함
-    List<Hospital> findByRoadNameAddressStartsWith(String keyword); // 시작
-    List<Hospital> findByRoadNameAddressEndingWith(String keyword); // 끝남
-    List<Hospital> findByHospitalNameStartsWith(String keyword);
+    List<Hospital> findByHospitalNameStartsWith(String keyword); // 시작
+    List<Hospital> findByHospitalNameEndsWith(String keyword); // 끝남
+
+    List<Hospital> findByPatientRoomCountGreaterThanAndPatientRoomCountLessThan(int var1, int var2);
+    List<Hospital> findByPatientRoomCountBetween(int var1, int var2);
+
+    List<Hospital> findByPatientRoomCountBetweenOrderByPatientRoomCountDesc(int a, int b);
+
 }
