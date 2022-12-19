18bc638dbef5eaae1b8c7eb9d110e3134dd2fdcd
sangho527 <sy980527@naver.com>
2022-12-19 20:52:51 +0900

    [UPDATE] 221219

---
+++ src/main/java/com/mustache/springbootmustache1111/SpringbootMustache1111Application.java
@@ -0,0 +1,13 @@
+package com.mustache.springbootmustache1111;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMustache1111Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMustache1111Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/mustache/springbootmustache1111/SpringbootMustache1111ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mustache.springbootmustache1111;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMustache1111ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
